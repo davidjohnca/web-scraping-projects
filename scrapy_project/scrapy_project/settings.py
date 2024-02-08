f3bbc2fb36da48e6f8ed30fd56abe4879e9c3294
davidjohnca <112810154+davidjohnca@users.noreply.github.com>
2024-02-08 17:15:04 +0000

    Update settings.py

--- scrapy_project/scrapy_project/settings.py
+++ scrapy_project/scrapy_project/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = "scrapy_project.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "scrapy_project (+http://www.yourdomain.com)"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
